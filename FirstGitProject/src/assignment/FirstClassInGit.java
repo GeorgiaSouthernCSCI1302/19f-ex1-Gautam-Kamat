607e47f3c5ac6f8b062cb41691cb9e3279498399
gl01487 <gl01487@CEIT2553214X006.ad.georgiasouthern.edu>
2019-08-21 16:57:01 -0400

    finished

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,4 +1,5 @@
 package assignment;
+import java.util.Scanner;
 
 public class FirstClassInGit {
 
@@ -6,9 +7,16 @@ public class FirstClassInGit {
 		System.out.println("Hello World!");
 		
 		//add a statement to display your name in the console
-		System.out.println("Gautam Lakshmanan");
+		System.out.println("Gautam");
 		//Then commit your changes
+		
+		Scanner nsa = new Scanner(System.in);
 
+		String x = nsa.nextLine();
+		
+		System.out.println("Type in your last name");
+		
+		System.out.println("Gautam" + x);
 	}
 
 }
